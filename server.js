30067190b790158b677472ac40e568774cbf9ed1
piman <44853490+piman51277@users.noreply.github.com>
2021-02-16 09:27:18 -0500

    generate home page based off contents of public

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
 const express = require('express')
 const app = express()
-const port = 80
+
+//generate a list of pages based on the contents of public
+const fs = require('fs')
+const pageList = fs.readdirSync('./public');
+let pagesLinks = ''
+for (i in pageList) {
+    pagesLinks += `<a href="/${pageList[i]}">${pageList[i]}</a><br>`
+}
 
 app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+    let contents = `<!DOCTYPE html><html><head><title>Calculator pages</title></head><body><h1>Welcome to the Calculator pages, a collection of various calculator webpages</h1>${pagesLinks}</body></html>`
+    res.send(contents)
+})
 
 app.listen(80, () => {})
